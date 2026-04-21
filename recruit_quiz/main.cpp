bc4cdd5d8cee2712f3e3a423b4f88b6c466fe29d
sho1720 <izumi1715shodai@gmail.com>
2026-04-21 12:14:52 +0900

    問題文をランダムにした

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <random>
 using namespace std;
 
 //問題文と答えをまとめる構造体
@@ -10,12 +11,30 @@ struct Question
 };
 
 int main() {
-	Question questions[] =
-	{
-		{"13x(-5)", 13 * (-5) },
-		{"(-21)/(-3)", -21 / -3},
-		{"7-(4+2)/2", 7 - (4 + 2) / 2},
-	};
+	Question questions[3];
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	//掛け算
+	int x = uniform_int_distribution<>(1, 30)(rand);
+	int y = uniform_int_distribution<>(1, 20)(rand);
+	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].a = x * y;
+
+	//割り算
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[1].q = to_string(x) + "/" + to_string(y);
+	questions[1].a = x / y;
+
+	//複雑な式
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 100)(rand);
+	int z = uniform_int_distribution<>(1, 10)(rand);
+	int w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = to_string(x) + "-(" + to_string(y) + "+" + to_string(z) + ")/" + to_string(w);
+	questions[2].a = x - (y + z) / w;
 
 	cout << "[リクルート試験対策クイズ]\n";
 	
