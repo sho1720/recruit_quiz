c6d9ca7f69cce3cbefbab2102e9e0aea009438da
sho1720 <izumi1715shodai@gmail.com>
2026-04-28 11:16:19 +0900

    球の体積問題を追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -53,6 +53,13 @@ int main() {
 		"底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ",
 		x * x * y /3});
 
+	//球の体積
+	x = uniform_int_distribution<>(1, 5)(rand) * 3;
+	//push_backで追加 追加される物は構造体Questionの初期化リストになる
+	questions.push_back({
+		"半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3とする。Xの値を求めよ",
+		x * x * x * 4 / 3 });
+
 	cout << "[リクルート試験対策クイズ]\n";
 	
 	//範囲ベースfor文
